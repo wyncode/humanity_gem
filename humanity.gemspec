4dd44d6746725d76f95c47f81a4154a0194d8f63
Andy Weiss <andyweiss1982@gmail.com>
2018-05-15 12:23:44 -0400

    active record

--- humanity.gemspec
+++ humanity.gemspec
@@ -29,7 +29,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_runtime_dependency 'pg', '~> 1.0'
-  spec.add_runtime_dependency 'activesupport', '~> 5.2'
+  spec.add_runtime_dependency 'activerecord', '~> 5.2'
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/humanity.rb
+++ lib/humanity.rb
@@ -1,21 +1,16 @@
 require "humanity/version"
 require "pg"
-require "active_support/core_ext/hash/keys"
-require "active_support/core_ext/hash/except"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(database: 'contacts', adapter: 'postgresql')
 
 module Humanity
-  class Person
-    @@connection = PG::Connection.open(dbname: 'contacts')
-    @@attributes = [:tummy, :emotion, :first_name, :last_name, :id]
-    @@public_attributes = [:tummy, :emotion, :first_name, :last_name]
-    attr_reader :id
-    attr_accessor *@@public_attributes
+  class Person < ActiveRecord::Base
 
-    def initialize(first_name: nil, last_name: nil, tummy: "grumbling", emotion: "sad")
-      @first_name = first_name
-      @last_name  = last_name
-      @tummy      = tummy
-      @emotion    = emotion
+    def initialize(attributes)
+      super
+      self.tummy = "grumbling"
+      self.emotion = "sad"
     end
 
     def skills
@@ -23,79 +18,9 @@ module Humanity
     end
 
     def eat_lunch
-      @tummy    = "full"
-      @emotion  = "happy"
+      self.tummy    = "full"
+      self.emotion  = "happy"
       self
-    end
-
-    def save
-      if self.id
-        conds = @@public_attributes
-                  .map{ |attr| "#{attr} = '#{send(attr)}'" }
-                  .join(", ")
-        sql   = "UPDATE people
-                SET #{conds}
-                WHERE id = #{self.id}
-                RETURNING #{@@attributes.join(', ')};"
-      else
-        values  = @@public_attributes
-                    .map{ |attr| "'#{send(attr)}'" }
-                    .join(', ')
-        sql     = "INSERT INTO people(#{@@public_attributes.join(', ')})
-                  VALUES(#{values})
-                  RETURNING #{@@attributes.join(', ')};"
-      end
-      record  = @@connection.exec(sql).first
-      @id     = record["id"]
-      self.class.from_record(record)
-    end
-
-    def delete
-      if self.id
-        @@connection.exec(
-          "DELETE FROM people
-          WHERE id = #{self.id}"
-        )
-      end
-      true
-    end
-
-    def attributes
-      attrs = {}
-      @@attributes.each do |attribute|
-        attrs[attribute.to_s] = send(attribute)
-      end
-      attrs
-    end
-
-    def self.find(id)
-      record = @@connection.exec(
-        "SELECT #{@@attributes.join(', ')} FROM people
-        WHERE id = #{id}"
-      ).first
-      self.from_record(record)
-    end
-
-    def self.all
-      @@connection.exec(
-        "SELECT #{@@attributes.join(', ')} FROM people;"
-      ).map{ |record| self.from_record(record) }
-    end
-
-    def self.where(conds)
-      conds = conds.map{ |key, value| "#{key} = '#{value}'" }.join(" AND ")
-      @@connection.exec(
-        "SELECT #{@@attributes.join(', ')} FROM people
-        WHERE #{conds};"
-      ).map{ |record| self.from_record(record) }
-    end
-
-    private
-
-    def self.from_record(record)
-      instance = self.new(record.symbolize_keys.except(:id))
-      instance.instance_variable_set(:@id, record["id"])
-      instance
     end
 
   end
