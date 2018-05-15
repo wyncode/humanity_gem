c4067d42ffefc9623cf62c5a14a82ef4519dc817
Andy Weiss <andyweiss1982@gmail.com>
2018-05-15 11:50:47 -0400

    basic orm

--- lib/humanity.rb
+++ lib/humanity.rb
@@ -1,12 +1,21 @@
 require "humanity/version"
+require "pg"
+require "active_support/core_ext/hash/keys"
+require "active_support/core_ext/hash/except"
 
 module Humanity
   class Person
-    attr_reader :tummy, :emotion
+    @@connection = PG::Connection.open(dbname: 'contacts')
+    @@attributes = [:tummy, :emotion, :first_name, :last_name, :id]
+    @@public_attributes = [:tummy, :emotion, :first_name, :last_name]
+    attr_reader :id
+    attr_accessor *@@public_attributes
 
-    def initialize
-      @tummy = "grumbling"
-      @emotion = "sad"
+    def initialize(first_name: nil, last_name: nil, tummy: "grumbling", emotion: "sad")
+      @first_name = first_name
+      @last_name  = last_name
+      @tummy      = tummy
+      @emotion    = emotion
     end
 
     def skills
@@ -14,8 +23,82 @@ module Humanity
     end
 
     def eat_lunch
-      @tummy = "full"
-      @emotion = "happy"
+      @tummy    = "full"
+      @emotion  = "happy"
+      self
+    end
+
+    def save
+      if self.id
+        conds = @@public_attributes
+                  .map{ |attr| "#{attr} = '#{send(attr)}'" }
+                  .join(", ")
+        sql   = "UPDATE people
+                SET #{conds}
+                WHERE id = #{self.id}
+                RETURNING #{@@attributes.join(', ')};"
+      else
+        values  = @@public_attributes
+                    .map{ |attr| "'#{send(attr)}'" }
+                    .join(', ')
+        sql     = "INSERT INTO people(#{@@public_attributes.join(', ')})
+                  VALUES(#{values})
+                  RETURNING #{@@attributes.join(', ')};"
+      end
+      record  = @@connection.exec(sql).first
+      @id     = record["id"]
+      self.class.from_record(record)
+    end
+
+    def delete
+      if self.id
+        @@connection.exec(
+          "DELETE FROM people
+          WHERE id = #{self.id}"
+        )
+        @id = nil
+      end
+      self
+    end
+
+    def attributes
+      attrs = {}
+      @@attributes.each do |attribute|
+        attrs[attribute] = send(attribute)
+      end
+      attrs
+    end
+
+    def self.find(id)
+      record = @@connection.exec(
+        "SELECT #{@@attributes.join(', ')} FROM people
+        WHERE id = #{id}"
+      ).first
+      self.from_record(record)
+    rescue
+      nil
+    end
+
+    def self.all
+      @@connection.exec(
+        "SELECT #{@@attributes.join(', ')} FROM people;"
+      ).map{ |record| self.from_record(record) }
+    end
+
+    def self.where(conds)
+      conds = conds.map{ |key, value| "#{key} = '#{value}'" }.join(" AND ")
+      @@connection.exec(
+        "SELECT #{@@attributes.join(', ')} FROM people
+        WHERE #{conds};"
+      ).map{ |record| self.from_record(record) }
+    end
+
+    private
+
+    def self.from_record(record)
+      instance = self.new(record.symbolize_keys.except(:id))
+      instance.instance_variable_set(:@id, record["id"])
+      instance
     end
 
   end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,7 +1,15 @@
 RSpec.describe Humanity::Person do
 
   let(:person) do
-    Humanity::Person.new
+    Humanity::Person.new(first_name: "Andy", last_name: "Weiss")
+  end
+
+  let(:other_person) do
+    Humanity::Person.new(first_name: "Other", last_name: "Person")
+  end
+
+  before do
+    PG::Connection.open(dbname: 'contacts').exec("DELETE FROM people;")
   end
 
   it "knows how to eat" do
@@ -34,6 +42,52 @@ RSpec.describe Humanity::Person do
       expect(person.emotion).to eq("happy")
     end
 
+  end
+
+  it "can persist to a database" do
+    person.save
+
+    expect(person.id).not_to eq(nil)
+  end
+
+  it "can be retrieved from a database" do
+    person.save
+
+    expect(Humanity::Person.find(person.id)).to be_a(Humanity::Person)
+  end
+
+  it "can be deleted from a database" do
+    person.save
+
+    person.delete
+
+    expect(person.id).to eq(nil)
+  end
+
+  it "can be retrieved as a collection" do
+    person.save
+    other_person.save
+
+    expect(Humanity::Person.all.map(&:id)).to match_array([person.id, other_person.id])
+  end
+
+  it "can be filtered as a collecton" do
+    person.save
+    other_person.save
+
+    expect(
+      Humanity::Person.where(first_name: "Andy").map(&:id)
+    ).to match_array([person.id])
+  end
+
+  it "can display its attributes" do
+    expected = {
+                  first_name: "Andy",
+                  last_name: "Weiss",
+                  tummy: "grumbling",
+                  emotion: "sad"
+                }
+    expect(person.attributes).to include(expected)
   end
 
 end
